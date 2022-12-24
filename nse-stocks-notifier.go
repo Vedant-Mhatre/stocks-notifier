cb635493179d209e618b98f05f027439805009dc
Vedant-Mhatre <vedantmh@gmail.com>
2022-12-24 23:31:55 +0530

    minor changes

--- nse-stocks-notifier.go
+++ nse-stocks-notifier.go
@@ -7,9 +7,44 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"strings"
+	"time"
 
 	"github.com/piquette/finance-go/quote"
 )
+
+func getDirectoryPath() (string, error) {
+	// Check if a directory path was provided
+	if len(os.Args) < 2 {
+		return "", fmt.Errorf("file path not provided")
+	}
+
+	// Get the directory path from the command-line argument
+	dir := os.Args[1]
+
+	// Check if the directory path is a valid directory
+	if fi, err := os.Stat(dir); err != nil || !fi.IsDir() {
+		return "", fmt.Errorf("'%s' is not a valid directory", dir)
+	}
+
+	// Return the directory path
+	return dir, nil
+}
+
+func directoryPathHelpMessage() {
+	fmt.Println("\nProvide the path where the stocks.json file is stored as an argument.\n\nThis file should contain the stock prices at which an alert is to be generated, as shown in the example below:")
+	fmt.Println("\nExample stocks.json file:")
+	fmt.Println(strings.TrimSpace(`
+{
+  "ICICIBANK": 880,
+  "HDFCBANK": 1600,
+  "INFY": 1500
+}`))
+	fmt.Println("\nTo run the program, use the following command:")
+	fmt.Printf("\n%s <directory>\n", os.Args[0])
+	fmt.Println("\nIf the stocks.json file is in the current directory, use '.' as the directory path.")
+	os.Exit(1)
+}
 
 func readJSONData(filename string) (map[string]interface{}, error) {
 	file, err := os.Open(filename)
@@ -51,7 +86,7 @@ func notify(title, text string) error {
 	return nil
 }
 
-func handleError(err error) {
+func notifyError(err error) {
 	fmt.Printf("Error: %v\n", err)
 	notify("Stock price alert", fmt.Sprintf("Error: %v", err))
 }
@@ -71,7 +106,7 @@ func GetStockPrice(symbol string) (float64, error) {
 			return 0, fmt.Errorf("failed to get stock quote due to network error: %v", err)
 		}
 
-		handleError(err)
+		notifyError(err)
 		return 0, fmt.Errorf("failed to get stock quote for symbol %q: %v", symbol, err)
 	}
 
@@ -96,23 +131,32 @@ func isNetworkError(err error) bool {
 
 func main() {
 
+	dir, error := getDirectoryPath()
+	if error != nil {
+		fmt.Println(error)
+		directoryPathHelpMessage()
+	}
+
 	var stocks map[string]interface{}
-	stocks, err := readJSONData("stocks.json")
+	stocks, err := readJSONData(dir + "/stocks.json")
 	if err != nil {
-		handleError(err)
+		notifyError(err)
 	}
 
-	for symbol := range stocks {
-		price, err := GetStockPrice(symbol + ".NS")
-		if err != nil {
-			handleError(err)
-			continue
+	for {
+		for symbol := range stocks {
+			price, err := GetStockPrice(symbol + ".NS")
+			if err != nil {
+				notifyError(err)
+				continue
+			}
+
+			notify("Stock price alert", fmt.Sprintf("Price of stock %v: %.2f", symbol, price))
+			fmt.Printf("Price of stock %q: %.2f\n", symbol, price)
 		}
-
-		// notify("Stock price alert", fmt.Sprintf("Price of stock %v: %.2f", symbol, price))
-		fmt.Printf("Price of stock %q: %.2f\n", symbol, price)
+		time.Sleep(10 * time.Second)
+		break
 	}
-
 	var mem runtime.MemStats
 	runtime.ReadMemStats(&mem)
 	fmt.Printf("Total Allocated = %v MiB", mem.TotalAlloc/1024/1024)
